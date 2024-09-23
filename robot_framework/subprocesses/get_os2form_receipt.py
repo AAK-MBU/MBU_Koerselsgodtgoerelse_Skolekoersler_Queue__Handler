1571a138a3632ada3027eaeb5bc4df167c9f1ccd
jakobkrog <jakoblkrog@pm.me>
2024-09-23 13:26:42 +0200

    updated readme and main process to handle business and robot exceptions.

--- robot_framework/subprocesses/get_os2form_receipt.py
+++ robot_framework/subprocesses/get_os2form_receipt.py
@@ -3,12 +3,13 @@ import json
 import os
 from mbu_dev_shared_components.os2forms import documents
 import requests
-from OpenOrchestrator.database.queues import QueueStatus
 
 
 def fetch_receipt(queue_element, os2_api_key, path, orchestrator_connection):
     """Fetch a receipt from OS2FORMS and save it to the specified path."""
     element_data = json.loads(queue_element.data)
+    filename = element_data['filename']
+    filename_without_ext = os.path.splitext(filename)[0]
     url = element_data.get('attachment')
     uuid = element_data.get('uuid')
 
@@ -20,11 +21,12 @@ def fetch_receipt(queue_element, os2_api_key, path, orchestrator_connection):
         # Download the file bytes
         file_content = documents.download_file_bytes(url, os2_api_key)
 
-        if not os.path.exists(path):
-            os.makedirs(path)
+        new_path = os.path.join(path, filename_without_ext)
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
 
         filename = f"receipt_{uuid}.pdf"
-        file_path = os.path.join(path, filename)
+        file_path = os.path.join(new_path, filename)
 
         # Save the file content
         with open(file_path, 'wb') as f:
@@ -37,5 +39,7 @@ def fetch_receipt(queue_element, os2_api_key, path, orchestrator_connection):
         raise RuntimeError(error_message) from e
 
     except OSError as e:
-        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.FAILED, "Failed to save receipt file.")
+        error_message = f"Error saving the file from OS2FORMS: {e}"
         raise RuntimeError(error_message) from e
+
+    return new_path
