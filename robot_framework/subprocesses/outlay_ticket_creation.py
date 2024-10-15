d4f92c3b1b0b3b9f998480a00ed35dbf463ff710
jakobkrog <jakoblkrog@pm.me>
2024-10-15 16:43:48 +0200

    Updated README to match the realted process.
    Changed to Queue Framework and modified it to match the process.

--- robot_framework/subprocesses/outlay_ticket_creation.py
+++ robot_framework/subprocesses/outlay_ticket_creation.py
@@ -49,20 +49,17 @@ def decrypt_cpr(element_data):
     return encryptor.decrypt(encrypted_cpr.encode('utf-8'))
 
 
-def handle_opus(queue_element, path, orchestrator_connection):
+def handle_opus(queue_element, path, browser, orchestrator_connection):
     """Handle the OPUS ticket creation process."""
-    from robot_framework.exceptions import BusinessError
 
     element_data = json.loads(queue_element.data)
     attachment_path = os.path.join(path, f'receipt_{element_data["uuid"]}.pdf')
 
-    browser = initialize_browser()
     navigate_to_opus(browser)
     fill_form(browser, element_data)
     upload_attachment(browser, attachment_path)
 
-    if not complete_form_and_submit(browser, element_data):
-        raise BusinessError("Kontrolfejl.")
+    complete_form_and_submit(browser, element_data)
 
     orchestrator_connection.log_trace("Successfully created outlay ticket.")
     print("Successfully created outlay ticket.")
@@ -71,7 +68,7 @@ def handle_opus(queue_element, path, orchestrator_connection):
 def navigate_to_opus(browser):
     """Navigate to OPUS page and open required tabs."""
     browser.get("https://ssolaunchpad.kmd.dk/?kommune=1574&start=portal")
-    WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@class='TabText_SmallTabs' and text()='Min Økonomi']")))
+    WebDriverWait(browser, 90).until(EC.presence_of_element_located((By.XPATH, "//div[@class='TabText_SmallTabs' and text()='Min Økonomi']")))
     wait_and_click(browser, By.XPATH, "//div[@class='TabText_SmallTabs' and text()='Min Økonomi']")
     wait_and_click(browser, By.XPATH, "//div[text()='Bilag og fakturaer']")
     wait_and_click(browser, By.XPATH, "/html/body/div[1]/table/tbody/tr[1]/td/div/div[1]/div[9]/div[2]/span[2]")
@@ -96,23 +93,23 @@ def fill_form(browser, element_data):
 def upload_attachment(browser, attachment_path):
     """Upload the attachment file to the browser form."""
     wait_and_click(browser, By.ID, 'WD0189')  # Click 'Vedhæft nyt' button
-    WebDriverWait(browser, 10).until(
+    WebDriverWait(browser, 20).until(
         lambda driver: driver.execute_script("return document.readyState") == "complete"
     )
     browser.switch_to.default_content()
     switch_to_frame(browser, 'URLSPW-0')
     wait_and_click(browser, By.XPATH, '/html/body/table/tbody/tr/td/div/div[1]/div/div[3]/table/tbody/tr/td/div/div/span/span[2]/form')  # Click 'Vælg fil' button
 
-    time.sleep(3)
+    time.sleep(4)
     keyboard = Controller()
     keyboard.type(attachment_path)
     time.sleep(2)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
-    time.sleep(1)
+    time.sleep(2)
 
     wait_and_click(browser, By.XPATH, '/html/body/table/tbody/tr/td/div/div[1]/div/div[4]/div/table/tbody/tr/td[3]/table/tbody/tr/td[1]/div')  # Click 'OK' button
-    time.sleep(1)
+    time.sleep(2)
 
 
 def press_key(keyboard, key):
@@ -123,6 +120,8 @@ def press_key(keyboard, key):
 
 def complete_form_and_submit(browser, element_data):
     """Complete the form and submit the ticket."""
+
+    from robot_framework.exceptions import BusinessError
 
     browser.switch_to.default_content()
     switch_to_frame(browser, 'contentAreaFrame')
@@ -151,20 +150,23 @@ def complete_form_and_submit(browser, element_data):
 
     # Check for business error here
     if not browser.find_elements(By.XPATH, "//*[contains(text(), 'Udgiftsbilag er kontrolleret og OK')]"):
-        return False
+        raise BusinessError("Fejl ved kontrol af udgiftsbilag.")
 
     wait_and_click(browser, By.ID, 'WD1B')  # Click 'Opret' button
-    return True
+    time.sleep(4)
+    if not browser.find_elements(By.XPATH, "//*[contains(text(), 'er oprettet')]"):
+        time.sleep(1)
+        raise BusinessError("Fejl ved oprettelse af udgiftsbilag, kontrol OK.")
 
 
 def switch_to_frame(browser, frame):
     """Switch to the required frames to access the form."""
-    WebDriverWait(browser, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, frame)))
+    WebDriverWait(browser, 30).until(EC.frame_to_be_available_and_switch_to_it((By.ID, frame)))
 
 
 def enter_text(browser, element_id, text):
     """Helper to enter text into a form element."""
-    input_element = WebDriverWait(browser, 10).until(
+    input_element = WebDriverWait(browser, 30).until(
         EC.presence_of_element_located((By.ID, element_id))
     )
     input_element.send_keys(text)
@@ -173,5 +175,5 @@ def enter_text(browser, element_id, text):
 def wait_and_click(browser, by, value):
     """Wait for an element to be clickable, then click it."""
 
-    WebDriverWait(browser, 10).until(EC.presence_of_element_located((by, value)))
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((by, value)))
     click_element_with_retries(browser, by, value)
